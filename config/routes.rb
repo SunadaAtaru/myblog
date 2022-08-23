1f21a89694660da9d5c3834de4ff878c3d56c366
SunadaAtaru <sleepingdoll2008@gmail.com>
2022-08-23 23:40:48 +0900

    投稿機能の実装途中

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # ここにusersコントローラーのshowアクションのルーティングを追加する
   get '/users/:id', to: 'users#show', as: 'user'
+
+  # ==========ここから追加する==========
+  resources :posts, only: %i(new create) do
+    resources :photos, only: %i(create)
+  end
+  # ==========ここまで追加する==========
+
 end
