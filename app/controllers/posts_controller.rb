bf86d09da5951b177890a23436178b4845cdd97a
SunadaAtaru <sleepingdoll2008@gmail.com>
2022-08-25 00:05:39 +0900

    投稿機能実装、ビューの手前

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,20 +6,20 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-    if post.photo.present?
+    if @post.photos.present?
       @post.save
       redirect_to root_path
       flash[:notice] = "投稿が保存されました"
     else
       redirect_to root_path
-      flash[:danger] = "投稿に失敗しました"
+      flash[:alert] = "投稿に失敗しました"
     end
   end
   
   private
 
   def post_params
-    params.require(:post).permit(:caption,photos_attributes:[:image]).merge(user_id:current_user.id)
+    params.require(:post).permit(:caption, photos_attributes: [:image]).merge(user_id: urrent_user.id)
   end
 
     
